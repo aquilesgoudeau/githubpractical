e698f8b802b98966803decf44b5bfdd4769fc078
Leo <goudeaulopez@yahoo.com>
2026-04-22 02:15:01 +1000

    nuevo archivo

---
+++ ejemplo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View,Text} from "react-native"
+
+const Ejemplo = () =>{
+    return(
+        <View>
+            <Text>Hola mundo</Text>
+        </View>
+    )
+}
+export default Ejemplo
