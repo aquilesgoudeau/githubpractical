08bc3d54179733adeeb010a039a55327657470c2
Leo <goudeaulopez@yahoo.com>
2026-04-26 06:55:27 +1000

    moviendo los archivos practical1

---
+++ practical1/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text} from "react-native"
+
+const App = () =>{
+    return(
+        <View>
+            <Text>Hola mundo!!!!</Text>
+            <Text>Chao mundo!!!!</Text>
+            <Text>aqui vamos</Text>
+        </View>
+    )
+}
+export default App
